526ee95ca35d0cf959c8ecc5017a7f994a628841
RobsonMaestro <robson_maestro@alumni.usp.br>
2022-09-10 12:05:14 -0300

    Inserção e Busca nas tabelas do banco de dados

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Este projeto foi desenvolvido baseado no curso de TypeORM da School Of Net.
     ```
     npx typeorm init --name TypeORM-start --database mysql
     ```
+## Utilização básica para manipulação dos dados no banco
+
+1. Conectando no banco
+2. Inserindo dados na tabela
+3. Buscando os dados inseridos
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,16 @@ import { User } from "./entity/User"
 
 AppDataSource.initialize().then(async () => {
 
-    console.log("Inserting a new user into the database...")
+    // Salva no banco de dados
     const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
+    user.firstName = "OUTRO"
+    user.lastName = "NOME"
+    user.age = 34
     await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    console.log("Usuario inserido com o Id: " + user.id)
 
-    console.log("Loading users from the database...")
+    // Busca no banco de dados
     const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    console.log("Listagem: ", users)
 
 }).catch(error => console.log(error))
